cd22034bf8f38b8fa43acfa47f7a49066ce9bd66
Ashni <ashni.khoenkhoen@student.hu.nl>
2019-02-25 14:56:52 +0100

    NS

--- Les 2/Final_assignment NS.py
+++ Les 2/Final_assignment NS.py
@@ -1,24 +1,29 @@
 Afstand = float (input('Afstand in kilometers:'))
 Leeftijd = int(input('Wat is uw leeftijd?'))
-Weekendrit = bool(input('Reist u in het weekend? (Ja=True/Nee=False):'))
+Weekendrit = (input('Reist u in het weekend? (Ja=True/Nee=False):'))
+Wkdrit = 0
+if Weekendrit == 'Ja':
+    Wkdrit = True
+if Weekendrit == 'Nee':
+    Wkdrit = False
 
 def standaardprijs (afstandKM):
     if afstandKM > 50:
-        tarief = 15 + (0,60 * afstandKM)
+        tarief = 15 + (0.60 * afstandKM)
     if afstandKM <= 0:
         tarief = 0
     else:
-        tarief = 0,80 * Afstand
+        tarief = 0.80 * Afstand
     return tarief
 
 def ritprijs(Leeftijd, Weekendrit, afstandKM):
-    if Weekendrit == False and 65 <= Leeftijd < 12:
-        tarief = float(standaardprijs(afstandKM) * 0.70)
-    if Weekendrit == True and 65 <= Leeftijd < 12:
-        tarief = float(standaardprijs(afstandKM) * 0.65)
-    if Weekendrit == True and 65 > Leeftijd >= 12:
-        tarief = float(standaardprijs(afstandKM) * 0.60)
-    if Weekendrit == False and 65 > Leeftijd >= 12:
-        tarief = float(standaardprijs(afstandKM))
-    return float(tarief)
-print('Uw treinrit kost: €' + str(float(ritprijs('Leeftijd', 'Weekendrit', 'Afstand'))))
+    if not Weekendrit and (Leeftijd >= 65 or Leeftijd < 12):
+        tarief = standaardprijs(afstandKM) * 0.70
+    if Weekendrit and (Leeftijd >= 65 or Leeftijd < 12):
+        tarief = standaardprijs(afstandKM) * 0.65
+    if Weekendrit and (Leeftijd < 65 or Leeftijd >= 12):
+        tarief = standaardprijs(afstandKM) * 0.60
+    if not Weekendrit and (Leeftijd < 65 or Leeftijd >= 12):
+        tarief = standaardprijs(afstandKM)
+    return tarief
+print('Uw treinrit kost: €{:0.2f}' .format(round(ritprijs(Leeftijd, Wkdrit, Afstand),2)))
---
+++ Les 2/pe4_15.py
@@ -0,0 +1,10 @@
+Graden_Celsius = float(input('Temperatuur in Celsius:'))
+def convert (temperatuureenheid):
+    Graden_Fahrenheit = Graden_Celsius * 1.8 + 32
+    return Graden_Fahrenheit
+print('Temperatuur in Fahrenheit:')
+
+def table(temperatuureenheid):
+    print('{:8} {:8}'.format('F', 'C'))
+    for i in range(-30, 50, 10):
+        print ('{:7} {:7.1f}'.format(i * 1,8 + 32
